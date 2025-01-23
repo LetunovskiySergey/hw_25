56b4c30ddb8710b1f60cc5468f562206942f3a0c
LetunovskiySergey <se1701@mail.ru>
2025-01-24 00:10:19 +0600

    Created Result Screen

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -5,6 +6,46 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final String playerChoice =
+        ModalRoute.of(context)?.settings.arguments as String;
+
+    List<String> options = ['Камень', 'Ножницы', 'Бумага'];
+
+    String opponentChoice = options[Random().nextInt(3)];
+
+    String result;
+    if (playerChoice == opponentChoice) {
+      result = 'Ничья';
+    } else if ((playerChoice == 'Камень' && opponentChoice == 'Ножницы') ||
+        (playerChoice == 'Ножницы' && opponentChoice == 'Бумага') ||
+        (playerChoice == 'Бумага' && opponentChoice == 'Камень')) {
+      result = 'Вы победили!';
+    } else {
+      result = 'Вы проиграли!';
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Результат'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Вы выбрали: $playerChoice', style: TextStyle(fontSize: 20)),
+            Text('Противник выбрал: $opponentChoice',
+                style: TextStyle(fontSize: 20)),
+            Text(result,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Ещё раз'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
