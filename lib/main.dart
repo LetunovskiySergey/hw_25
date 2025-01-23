f75437285ca76bdefd00b23b1c6e1e9057ab2790
LetunovskiySergey <se1701@mail.ru>
2025-01-23 23:55:57 +0600

    Created MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hw_25/my_app.dart';
 
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Камень, Ножницы, Бумага',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/result': (context) => ResultScreen(),
+      },
+    );
+  }
+}
